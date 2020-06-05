9004f5eedf39a832a1dbaa9fd6dae5d02a6a37e2
pendemic <mikeomskinner@aol.com>
2020-06-05 10:57:35 -0500

    Changed pattern format for App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';//Added {Component}
 import logo from './logo.svg';
 import './App.css';
 
+/*
 function App() {
   return (
     <div className="App">
@@ -22,5 +23,24 @@ function App() {
     </div>
   );
 }
+*/
+//export default App;
+export default class App extends Component {
+  //Above we have created a class called App that extends the functionality of the Component class.
 
-export default App;
+  //The export keyword makes the class available for use outside of the JS file where it is created (like an access modifier in C#)
+
+  constructor(){
+    //We are going to create state data for our component. To do that we need to create a construtor method. This method will get called when an object is created using this class. Said another way, this methos will be called when the component is initialized.
+    
+    //To ensure that we have all the necessary features from React to create a stateful component, wwe need to call the super() method.
+    //This super() calls the constructor for the component class in React
+    super();
+  }
+  render = () =>
+  //When using the fat arrow syntax the return keyword is not needed and curly braces around the body of the function is also not needed
+    <div>
+
+    </div>
+
+};
