eee1f13e1a351a4d0c79861f35b7ca9b4955ba62
pendemic <mikeomskinner@aol.com>
2020-06-05 11:24:30 -0500

    Added Feature 1 and 2

--- src/App.js
+++ src/App.js
@@ -30,17 +30,42 @@ export default class App extends Component {
 
   //The export keyword makes the class available for use outside of the JS file where it is created (like an access modifier in C#)
 
-  constructor(){
+  constructor(props){
     //We are going to create state data for our component. To do that we need to create a construtor method. This method will get called when an object is created using this class. Said another way, this methos will be called when the component is initialized.
     
     //To ensure that we have all the necessary features from React to create a stateful component, wwe need to call the super() method.
     //This super() calls the constructor for the component class in React
-    super();
+    super(props);
+    this.state = {
+      userName: "Mikeo",
+      todoItems: [
+        {action: "Do Homework", done: false},
+        {action: "Go to Dance Practice", done: false},
+        {action: "Pay Bills", done: true},
+        {action: "Play Super Smash Bros", done: false},
+        {action: "Fix Headlights", done: false},
+        {action: "Apply For Jobs", done: false}
+      ]
+    }
   }
   render = () =>
   //When using the fat arrow syntax the return keyword is not needed and curly braces around the body of the function is also not needed
     <div>
-
+      {
+        /*
+        Features 1) and 2)
+        */
+      }
+      <ToDoBanner
+      displayName = {this.state.userName}
+      tasks = {this.state.todoItems}
+      />
     </div>
 
+};
+export class ToDoBanner extends Component{
+  render = () =>
+  <h4 className = "bq-primary text-white text-center p-2">
+    {this.props.displayName}'s To Do List ({this.props.tasks.filter(task => !task.done).length} items to do)
+  </h4>
 };
